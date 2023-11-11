8922640d87daed49138c01d1e8b5988a9e987b0a
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2023-11-10 16:51:06 -0800

    feat: first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	DBURL := os.Getenv("DATABASE_URL")
+	fmt.Println(DBURL)
+}
