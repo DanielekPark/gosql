543e088be8e4b78bf43077094e07aaa2999c5f18
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2023-11-12 12:10:42 -0800

    feat: connected db to main.go

--- main.go
+++ main.go
@@ -2,28 +2,63 @@ package main
 
 import (
 	"database/sql"
-	getreq "db/api/get"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
 var db *sql.DB
 
+type Result struct {
+	id      int64
+	name    string
+	link    string
+	detials string
+	Types   string
+	tags    string
+}
+
 func main() {
 	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("failed to load env line 28 ", err)
+	}
 
-	//Creates a connection to the database
-	db, err = sql.Open("mysql", os.Getenv("DATABASE_URL"))
+	// Database connection
+	db, err = sql.Open("mysql", os.Getenv("DSN"))
 	if err != nil {
 		log.Fatal("failed to open db connection", err)
 	}
 
+	// Routes
 	router := gin.Default()
-	router.GET("/links", getreq.GetLinks)
+	router.GET("/search", GetLinks)
 
-	//Runs the router
+	// Run the router
 	router.Run()
+}
+
+func GetLinks(c *gin.Context) {
+	query := "SELECT name FROM links WHERE id < 20"
+	res, err := db.Query(query)
+	defer res.Close()
+	if err != nil {
+		log.Fatal("(GetLinks) db.Query", err)
+	}
+
+	results := []Result{}
+	for res.Next() {
+		var link Result
+		err := res.Scan(&link.Types)
+		if err != nil {
+			log.Fatal("(GetLinks) res.Scan", err)
+		}
+		results = append(results, link)
+	}
+
+	c.JSON(http.StatusOK, results)
 }
