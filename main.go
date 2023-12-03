0818171c3be23d8730228fafd967f3e99bf42ded
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2023-12-03 10:42:02 -0800

    feat: added post route

--- main.go
+++ main.go
@@ -37,13 +37,14 @@ func main() {
 	// Routes
 	router := gin.Default()
 	router.GET("/search", GetLinks)
+	router.POST("/search", CreateLink)
 
 	// Run the router
 	router.Run()
 }
 
 func GetLinks(c *gin.Context) {
-	query := "SELECT * FROM links WHERE id < 20"
+	query := "SELECT * FROM links ORDER BY ID DESC LIMIT 1"
 	res, err := db.Query(query)
 	defer res.Close()
 	if err != nil {
@@ -61,4 +62,28 @@ func GetLinks(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, results)
+}
+
+func CreateLink(c *gin.Context) {
+	var newLink Result
+	err := c.BindJSON(&newLink)
+	if err != nil {
+		log.Fatal("(CreateLink) c.BindJSON ", err)
+	}
+
+	// POST Request
+	query := `INSERT INTO links (id, name, link, details, type, tags) VALUES (?, ?, ?, ?, ?, ?)`
+	//null, "Render", "https://render.com/", "Unified cloud to build and run all your apps and auto deploys from Git.", "Frontend Backend", "Render website host hosting api database apps free auto deploy"
+
+	res, err := db.Exec(query, newLink.Id, newLink.Name, newLink.Link, newLink.Details, newLink.Types, newLink.Tags)
+
+	if err != nil {
+		log.Fatal("(CreateLink) db.Exec", err)
+	}
+	newLink.Id, err = res.LastInsertId()
+	if err != nil {
+		log.Fatal("(CreateLink) res.LastInsertId", err)
+	}
+
+	c.JSON(http.StatusOK, newLink)
 }
